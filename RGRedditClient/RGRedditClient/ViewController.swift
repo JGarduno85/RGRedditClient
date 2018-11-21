449f3557e85921a3ba992db8811c5fb6f2d92756
JoseHumbertoPartidaGarduno <humbertusa79@gmail.com>
2018-11-20 22:16:49 -0800

    Chore: initialCommit, create project

---
+++ RGRedditClient/RGRedditClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGRedditClient
+//
+//  Created by Jose Humberto Partida Garduno on 11/20/18.
+//  Copyright © 2018 Jose Humberto Partida Garduno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
